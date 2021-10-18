77bba24df08b9d600fe7dca994fceb71cb4e627d
huy <huyn102010@gmail.com>
2021-10-17 22:27:13 -0400

    finished

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,14 +1,48 @@
-exports.toRoman = function(num) {
+exports.toRoman = function(num) 
+{
 
-  // # 1. Write a method TO_ROMAN, TO_ROMAN takes in INPUT_NUMBER (an arabic number)
-  // # 2. Create a OUTPUT string, set to ''
-  // # 3. Create a ROMAN_NUMERAL_TO_ARABIC_MAP that includes roman numerals as keys, arabic numbers as values
-  // # 4. Iterate over ROMAN_NUMERAL_TO_ARABIC_MAP, keep track of ROMAN_NUMERAL and ARABIC_NUMBER
-  // # 5. Set EVENLY_DIVISIBLE_TIMES = INPUT_NUMBER / ARABIC_NUMBER:
-  // # 6. If EVENLY_DIVISIBLE_TIMES >= 1
-  //   # 6a. Append ROMAN_NUMERAL to OUTPUT EVENLY_DIVISIBLE_TIMES
-  //   # 6b. Subtract ARABIC_NUMBER from INPUT_NUMBER EVENLY_DIVISIBLE_TIMES
-  // # 7. Return OUTPUT
+  var lookup = {'M':1000,
+                'CM':900,
+                'D':500,
+                'CD':400,
+                'C':100,
+                'XC':90,
+                'L':50,
+                'XL':40,
+                'X':10,
+                'IX':9,
+                'V':5,
+                'IV':4,
+                'I':1}, // lookup array
+  roman = ''; // empty string 
+
+  if(num<=0)
+  {
+    console.log('Please enter an Arabic number greater than 0!!')
+  }
+
+  // if(num>3000)
+  // {
+  //   console.log('Anything greater than 3000 might not be accurately presented now!!!')
+  // }
+
+  else
+  {
+    for ( i in lookup ) 
+    {
+      while ( num >= lookup[i] ) // initiate while loop 
+      {
+        roman += i;
+        num -= lookup[i]; // need this to get out of while loop 
+      }
+    }
+  return roman;
+  }
+  
 
 
 };
+
+
+
+

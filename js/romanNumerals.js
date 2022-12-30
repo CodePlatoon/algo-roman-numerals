3905f3e1df977c7829f1be3684daba3e155b2732
mickey08jk <mckee.marinecorps@gmail.com>
2022-12-30 09:55:11 -0600

    added dictionary for roman num and loop to str concat

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,26 @@
 exports.toRoman = function(num) {
+    let roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    let romanNumeral = '';
 
+    for (let i of Object.keys(roman)) {
+        let floorNum = Math.floor(num / roman[i]);
+        num -= floorNum * roman[i];
+        romanNumeral += i.repeat(floorNum)
+    };
+
+    return romanNumeral;
 };
